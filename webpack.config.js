f9012186241ee3d8362b92d2fe5f9e722a006193
Nolan Reilly <nolanreilly02@gmail.com>
2024-05-27 10:48:24 -0500

    Add Webpack to project

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        bundle: path.resolve(__dirname, "src/index.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Todo List",
+            filename: "index.html",
+            template: "src/template.html"
+        })
+    ]
+}
